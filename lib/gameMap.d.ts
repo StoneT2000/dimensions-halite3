e30c331808a36d7aa89552bb0655bf0c222df74b
StoneT2000 <stoneztao@gmail.com>
2020-03-31 13:03:34 -0700

    remove

--- lib/gameMap.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class GameMap {
-    map: never[];
-    players: never[];
-    constructor(width: number, height: number);
-}
--- lib/model/Enumerated.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type class_id<T> = number & T;
-export declare class Enumerated<T> {
-    id: class_id<T>;
-    /**
-     * Must be supered
-     */
-    constructor(id: class_id<T>);
-}
-export declare type ID_Map<T, V> = Map<class_id<T>, V>;
