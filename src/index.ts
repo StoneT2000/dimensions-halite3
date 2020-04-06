9ed9b5dc1423adc5d3538ea2a07a23d662397129
StoneT2000 <stoneztao@gmail.com>
2020-04-06 00:49:46 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -129,11 +129,6 @@ export default class Halite3Design extends Design {
      */
 
      // change timeout options of engine after "turn 0" as initialization bots have 10 seconds
-    match.matchEngine.setEngineOptions({
-      timeout: {
-        max: 2000
-      }
-    })
     
     let numPlayers = match.agents.length;
     
@@ -254,6 +249,11 @@ export default class Halite3Design extends Design {
         game.replay.players.set(player_id, player);
       })
       match.log.info(`Player initialization complete`);
+      match.matchEngine.setEngineOptions({
+        timeout: {
+          max: 2000
+        }
+      })
     }
 
     
